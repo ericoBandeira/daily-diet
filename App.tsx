6383bb6fa4f553d8cc9808b9213cbfc8c3a0f86b
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-09-27 17:12:44 -0300

    Fix some bugs

--- App.tsx
+++ App.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import { Home } from "@screens/Home";
+import theme from "@theme/index";
 import { StatusBar } from "react-native";
-import { Home } from "./src/screens/Home";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Home />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Home />
+      </>
+    </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
